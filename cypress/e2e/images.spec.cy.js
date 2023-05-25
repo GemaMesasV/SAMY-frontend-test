d86fed76c8c6a6277a0e94ffd7720eb16ac6f359
gemamesasv <gema.mesas.v@gmail.com>
2023-05-26 00:41:09 +0200

    fix(cypress): change wrong cy.get

--- cypress/e2e/images.spec.cy.js
+++ cypress/e2e/images.spec.cy.js
@@ -10,38 +10,50 @@ describe("images spec", () => {
     cy.get("#card > :nth-child(1)").should("exist");
   });
   it("user likes an image", () => {
-    cy.get(":nth-child(1) > #likeNumber").then(($likeNumber) => {
-      const previousLikes = parseInt($likeNumber.text());
+    cy.get(":nth-child(1) > .card__footer > .card__like > #likeNumber").then(
+      ($likeNumber) => {
+        const previousLikes = parseInt($likeNumber.text());
 
-      cy.get(":nth-child(1) > #likeIcon").click();
+        cy.get(
+          ":nth-child(1) > .card__footer > .card__like > #likeIcon"
+        ).click();
 
-      cy.get(":nth-child(1) > #likeNumber").should(($newLikeNumber) => {
-        const newLikes = parseInt($newLikeNumber.text());
-        expect(newLikes).to.equal(previousLikes + 1);
-      });
-    });
+        cy.get(
+          ":nth-child(1) > .card__footer > .card__like > #likeNumber"
+        ).should(($newLikeNumber) => {
+          const newLikes = parseInt($newLikeNumber.text());
+          expect(newLikes).to.equal(previousLikes + 1);
+        });
+      }
+    );
   });
   it("user unlikes an image", () => {
-    cy.get(":nth-child(2) > #likeNumber").then(($likeNumber) => {
-      const previousLikes = parseInt($likeNumber.text());
+    cy.get(":nth-child(2) > .card__footer > .card__like > #likeNumber").then(
+      ($likeNumber) => {
+        const previousLikes = parseInt($likeNumber.text());
 
-      cy.get(":nth-child(2) > #likeIcon").click();
+        cy.get(
+          ":nth-child(2) > .card__footer > .card__like > #likeIcon"
+        ).click();
 
-      cy.get(":nth-child(2) > #likeNumber").should(($newLikeNumber) => {
-        const newLikes = parseInt($newLikeNumber.text());
-        expect(newLikes).to.equal(previousLikes - 1);
-      });
-    });
+        cy.get(
+          ":nth-child(2) > .card__footer > .card__like > #likeNumber"
+        ).should(($newLikeNumber) => {
+          const newLikes = parseInt($newLikeNumber.text());
+          expect(newLikes).to.equal(previousLikes - 1);
+        });
+      }
+    );
   });
   it("user does a search", () => {
     cy.get("#searchInput").type("Grey beach");
     cy.get("#searchInput").type("{enter}");
-    cy.get(".main__cards--item--title").contains("Grey beach");
+    cy.get(".card").contains("Grey beach");
   });
   it("user does a search and goes to home", () => {
     cy.get("#searchInput").type("Grey beach");
     cy.get("#searchInput").type("{enter}");
-    cy.get(".main__cards--item--title").contains("Grey beach");
+    cy.get(".card").contains("Grey beach");
     cy.get("#headerLogo").click();
     cy.get(":nth-child(15)").should("exist");
   });
