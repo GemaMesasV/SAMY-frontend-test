503ba29d57443d2ad575ffcf58a4f846e5ede53f
gemamesasv <gema.mesas.v@gmail.com>
2023-05-31 02:20:53 +0200

    fix: fix get images usecase

--- src/usecases/get-images.usecase.js
+++ src/usecases/get-images.usecase.js
@@ -5,7 +5,7 @@ const IMAGES_PER_PAGE = 6;
 export class GetImagesUseCase {
   static async execute(page = 1) {
     const repository = new ImagesRepository();
-    const images = await repository.getImages();
+    const images = await repository.getImages(page);
     return images.slice((page - 1) * IMAGES_PER_PAGE, page * IMAGES_PER_PAGE);
   }
 }
